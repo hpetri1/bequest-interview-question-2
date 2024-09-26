2db2820a9587723a5cabc4ea867a1671d85bf54c
hpetri1 <39070807+hpetri1@users.noreply.github.com>
2024-09-26 06:29:24 -0600

    Add type safety to server, add helmet

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,24 +1,36 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
+import helmet from "helmet";
+import { Database } from "./types.js";
 
 const PORT = 8080;
 const app = express();
-const database = { data: "Hello World" };
+const database: Database = { data: "Hello World" };
 
-app.use(cors());
+app.use(helmet());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: "GET,POST",
+  })
+);
 app.use(express.json());
 
 // Routes
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response): void => {
   res.json(database);
 });
 
-app.post("/", (req, res) => {
-  database.data = req.body.data;
-  res.sendStatus(200);
+app.post("/", (req: Request, res: Response): void => {
+  if (typeof req.body.data === "string") {
+    database.data = req.body.data;
+    res.sendStatus(200);
+  } else {
+    res.status(400).json({ error: "Invalid data format" });
+  }
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, (): void => {
   console.log("Server running on port " + PORT);
 });
